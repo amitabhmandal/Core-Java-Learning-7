07db873a33f19765625f9f541054a8a1e5806154
amitabhmandal <amitabhmandal@192.168.1.12>
2018-07-17 06:06:28 +0530

    IdentityHashMap

--- src/main/java/collection/learning/map/WeakHashMapDemo.java
+++ src/main/java/collection/learning/map/WeakHashMapDemo.java
@@ -1,11 +1,38 @@
 package collection.learning.map;
 
+import java.util.HashMap;
+
 public class WeakHashMapDemo {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        operationWithHashMap();
 
     }
-    
 
+    public static void operationWithHashMap() {
+        HashMap m = new HashMap();
+
+        Temp t = new Temp();
+
+        m.put(t, "durga");
+        System.out.println(m);
+
+        // this does not have any reference
+        t = null;
+
+        System.gc();
+
+    }
+
+}
+
+class Temp {
+
+    public String toString() {
+        return "temp";
+    }
+
+    public void finalize() {
+        System.out.println("Finalize method called");
+    }
 }
